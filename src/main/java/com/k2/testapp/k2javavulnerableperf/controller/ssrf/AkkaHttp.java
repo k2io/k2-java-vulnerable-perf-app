3feae6af7ced657a6d957fc601a2faac05cc45f2
Harshit Singh Lodha <harshit@k2io.com>
2020-05-28 10:47:54 +0530

    Support for SSRF APIs.

---
+++ src/main/java/com/k2/testapp/k2javavulnerableperf/controller/ssrf/AkkaHttp.java
@@ -0,0 +1,78 @@
+package com.k2.testapp.k2javavulnerableperf.controller.ssrf;
+
+import akka.actor.ActorSystem;
+import akka.http.javadsl.Http;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Map;
+import java.util.concurrent.CompletionStage;
+
+@RestController
+@RequestMapping("/ssrf/akka")
+public class AkkaHttp {
+
+    public static final String EMPTY = "";
+    public static final String COUNT = "count";
+    public static final String url = "url";
+    public static final String URL_PARAM_NOT_FOUND = "url param not found";
+    public static final String ERROR_WHILE_FETCHING_URL_S_S_S = "Error while fetching url : %s : %s : %s";
+
+    private String connect(String url) {
+        String response = EMPTY;
+        try {
+            ActorSystem system = ActorSystem.create("test-system");
+            HttpRequest request = HttpRequest.create(url);
+            CompletionStage<HttpResponse> responseFuture =
+                    Http.get(system)
+                            .singleRequest(request);
+
+            HttpResponse httpResponse = responseFuture.toCompletableFuture().get();
+            response = String.valueOf(httpResponse.status().intValue());
+        } catch (Exception e) {
+            return String.format(ERROR_WHILE_FETCHING_URL_S_S_S, url, e.getMessage(), e.getCause());
+        }
+        return response;
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String connectQueryParam(@RequestParam String url, @RequestParam(defaultValue = "1") long count) {
+        String output = EMPTY;
+        if (count < 1) {
+            count = 1;
+        }
+        for (long i = 0; i < count; i++) {
+            output = connect(url);
+        }
+        return output;
+    }
+
+    @RequestMapping(method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public String connectByBody(@RequestParam Map<String, String> paramMap) {
+        String output = EMPTY;
+        long count = 1;
+        if (paramMap.containsKey(COUNT)) {
+            count = Long.parseLong(paramMap.get(COUNT));
+        }
+        if (count < 1) {
+            count = 1;
+        }
+        if (paramMap.containsKey(url)) {
+            for (long i = 0; i < count; i++) {
+                output = connect(paramMap.get(url));
+            }
+        } else {
+            throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST, URL_PARAM_NOT_FOUND);
+        }
+        return output;
+    }
+}
---
+++ src/main/java/com/k2/testapp/k2javavulnerableperf/controller/ssrf/ApacheHttp4.java
@@ -0,0 +1,73 @@
+package com.k2.testapp.k2javavulnerableperf.controller.ssrf;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/ssrf/apachehttp4")
+public class ApacheHttp4 {
+
+    public static final String EMPTY = "";
+    public static final String COUNT = "count";
+    public static final String url = "url";
+    public static final String URL_PARAM_NOT_FOUND = "url param not found";
+    public static final String ERROR_WHILE_FETCHING_URL_S_S_S = "Error while fetching url : %s : %s : %s";
+
+    private String connect(String url) {
+        String response = EMPTY;
+        try {
+            HttpClient client = HttpClientBuilder.create().build();
+            HttpResponse httpResponse = client.execute(new HttpGet(url));
+            int statusCode = httpResponse.getStatusLine().getStatusCode();
+            response = String.valueOf(statusCode);
+        } catch (Exception e) {
+            return String.format(ERROR_WHILE_FETCHING_URL_S_S_S, url, e.getMessage(), e.getCause());
+        }
+        return response;
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String connectQueryParam(@RequestParam String url, @RequestParam(defaultValue = "1") long count) {
+        String output = EMPTY;
+        if (count < 1) {
+            count = 1;
+        }
+        for (long i = 0; i < count; i++) {
+            output = connect(url);
+        }
+        return output;
+    }
+
+    @RequestMapping(method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public String connectByBody(@RequestParam Map<String, String> paramMap) {
+        String output = EMPTY;
+        long count = 1;
+        if (paramMap.containsKey(COUNT)) {
+            count = Long.parseLong(paramMap.get(COUNT));
+        }
+        if (count < 1) {
+            count = 1;
+        }
+        if (paramMap.containsKey(url)) {
+            for (long i = 0; i < count; i++) {
+                output = connect(paramMap.get(url));
+            }
+        } else {
+            throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST, URL_PARAM_NOT_FOUND);
+        }
+        return output;
+    }
+}
